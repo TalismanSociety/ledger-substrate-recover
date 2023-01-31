88279fe1acec37cc41ae7bfa1176402519f69060
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-02-01 00:57:34 +0900

    fix: account type

--- src/util/getJsonFromPair.ts
+++ src/util/getJsonFromPair.ts
@@ -3,6 +3,6 @@ import { Keypair } from "@polkadot/util-crypto/types"
 
 export const getJsonFromPair = (pair: Keypair, password: string) => {
   const keyring = new Keyring({ type: "sr25519" })
-  const acc = keyring.addFromPair(pair)
+  const acc = keyring.addFromPair(pair, { name: "Ledger Recover" }, "ed25519")
   return acc.toJson(password)
 }
